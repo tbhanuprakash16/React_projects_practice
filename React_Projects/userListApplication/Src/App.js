aa4726d73585759ebe2ed8f602c048f6a89e6d1f
tbhanuprakash16 <131454581+tbhanuprakash16@users.noreply.github.com>
2024-03-09 13:31:53 +0530

    Update App.js

--- React_Projects/userListApplication/Src/App.js
+++ React_Projects/userListApplication/Src/App.js
@@ -4,21 +4,25 @@ import './App.css'
 
 const userDetailsList = [
   {
+    uniqueNo: 1,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
     name: 'Esther Howard',
     role: 'software Developer',
   },
   {
+    uniqueNo: 2,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
     name: 'Floyd Miles',
     role: 'software Developer',
   },
   {
+    uniqueNo: 3,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
     name: 'Jacob Jones',
     role: 'software Developer',
   },
   {
+    uniqueNo: 4,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
     name: 'Devon Lane',
     role: 'software Developer',
@@ -30,7 +34,7 @@ const App = () => (
     <h1 className="heading">Users List</h1>
     <ul>
       {userDetailsList.map(eachUser => (
-        <UserProfile userDetails={eachUser} />
+        <UserProfile userDetails={eachUser} key = {eachUser.uniqueNo} />
       ))}
     </ul>
   </div>
